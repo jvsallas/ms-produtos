83d18be6ddfbd663b0f0585ef30009c2231aeecf
jvsallas <joaovictorsallas@gmail.com>
2022-11-15 14:58:25 -0300

    Add PropertyNamingStrategy.SnakeCaseStrategy no CarrinhoSaida

--- src/main/java/br/com/mercadosallas/carrinho/model/CarrinhoSaida.java
+++ src/main/java/br/com/mercadosallas/carrinho/model/CarrinhoSaida.java
@@ -1,19 +1,21 @@
 package br.com.mercadosallas.carrinho.model;
 
 import br.com.mercadosallas.produtos.dto.ProdutoDto;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class CarrinhoSaida {
     private Long id;
     private String idCliente;
